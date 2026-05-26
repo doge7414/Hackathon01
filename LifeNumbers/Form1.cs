7b298a36ad2d845f5ea5dbfc2295acf51cd7f732
Yu <kyyu0601@gmail.com>
2026-05-26 16:33:33 +0800

    .

---
+++ LifeNumbers/Form1.cs
@@ -0,0 +1,10 @@
+namespace LifeNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NumberGusseing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NumberGusseing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
